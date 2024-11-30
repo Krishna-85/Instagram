d273504c349f9384348ba0eb8876b0d80617ec27
krishna-85 <vaibhavchauhan658@gmail.com>
2024-11-30 18:51:22 +0530

    updated interface

--- script.js
+++ script.js
@@ -1,116 +1,136 @@
-const users = [
+
+var users = [
   {
-    name: "Sheriyansh",
-    profile_pic:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR--YX9-g5P0TPHMLLRi375nptFMBXkurT29w&s",
-    post_image: "https://i.ytimg.com/vi/pOGLAlGCKFE/maxresdefault.jpg",
-    likes: "View likes",
-    description:
-      "Sheriyansh coding school Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      "username": "Shraddhakapoor",
+      "userlocation": "Paris, France",
+      "userprofile": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202410/shraddha-kapoor-141825285-16x9_0.jpg?VersionId=APQQ9boRsLXjCFHLSUPkaAfN72Bdw5So",
+      "userPost": "https://drishtiandzahabia.com/cdn/shop/files/837ecddb-c18f-43e8-8388-c79f094efd5f.jpg?v=1695108186",
+      "like": true,
+      "likeCount": 1200,
+      "commentCount": 45,
+      "shareCount": 30,
+      "caption": "Red Chilli. #ParisVibes #TravelGoals",
+      "timeAgoUploaded": 5 
   },
   {
-    name: "Lakshaya Chaudhary",
-    profile_pic:
-      "https://yt3.googleusercontent.com/ERiTjj0Nq78Kc4fi6arZgehKdJYiLyl9lHRe9SIPBAX1jodqt3EwyQTT-b06Wn0NKOGASzpRqD4=s900-c-k-c0x00ffffff-no-rj",
-    post_image:
-      "https://yt3.googleusercontent.com/ERiTjj0Nq78Kc4fi6arZgehKdJYiLyl9lHRe9SIPBAX1jodqt3EwyQTT-b06Wn0NKOGASzpRqD4=s900-c-k-c0x00ffffff-no-rj",
-    likes: "View likes",
-    description:
-      "Lakshaya Chaudhary Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      "username": "Narendramodi",
+      "userlocation": "Mumbai, India",
+      "userprofile": "https://yt3.googleusercontent.com/NYTMmmC6waUzkAsWHXpIJGfUG4HOvZqszHsUI0Los1pgSXsd4XKxALEXimATCpaSXDB_A4LoGg=s900-c-k-c0x00ffffff-no-rj",
+      "userPost": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/Modi_yogi_together_1200x768.jpeg?VersionId=tJvD2_ESMs1KnpDXTL2HAudkNKkQZw2U",
+      "like": false,
+      "likeCount": 530,
+      "commentCount": 20,
+      "shareCount": 12,
+      "caption": "Double engine Government❤ #modisarkar",
+      "timeAgoUploaded": 12
   },
   {
-    name: "Yogi Adityanath",
-    profile_pic:
-      "https://bsmedia.business-standard.com/_media/bs/img/misc/2023-03/30/full/20230306047L.jpg",
-    post_image:
-      "https://bsmedia.business-standard.com/_media/bs/img/misc/2023-03/30/full/20230306047L.jpg",
-    likes: "View likes",
-    description:
-      "Yogi Adityanath Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      "username": "Sheryiansh_coding_school",
+      "userlocation": "Bhopal, India",
+      "userprofile": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR--YX9-g5P0TPHMLLRi375nptFMBXkurT29w&s",
+      "userPost": "https://i.ytimg.com/vi/pOGLAlGCKFE/maxresdefault.jpg",
+      "like": true,
+      "likeCount": 800,
+      "commentCount": 60,
+      "shareCount": 25,
+      "caption": "Boost your skill. #bootcamp #kodr",
+      "timeAgoUploaded": 24
   },
   {
-    name: "Shraddha Kapoor",
-    profile_pic: "https://images.indianexpress.com/2024/10/Shraddha-Kapoor.jpg",
-    post_image:
-      " https://drishtiandzahabia.com/cdn/shop/files/837ecddb-c18f-43e8-8388-c79f094efd5f.jpg?v=1695108186",
-    likes: "View likes",
-    description:
-      "Shraddha Kapoor ❤️ Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      "username": "artsy_corner",
+      "userlocation": "London, UK",
+      "userprofile": "https://images.unsplash.com/photo-1647205552933-12e3dfdb77b5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "userPost": "https://plus.unsplash.com/premium_photo-1692650759141-ed71d789ef1b?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "like": true,
+      "likeCount": 950,
+      "commentCount": 78,
+      "shareCount": 40,
+      "caption": "New piece in the gallery. Let me know your thoughts! 🎨 #AbstractArt",
+      "timeAgoUploaded": 7
   },
   {
-    name: "Narendra Modi",
-    profile_pic:
-      " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEYEI5nU2R_FOfpmEBKHJW7TXrT7PKTdmtAw&shttps://governancenow.com/temp/yogi_adityanath.jpg",
-    post_image:
-      "https://th-i.thgim.com/public/incoming/yaijra/article68277857.ece/alternates/FREE_1200/IMG_PM_Modi_takes_charge_2_1_5SCTKQL7.jpg",
-    likes: "View likes",
-    description:
-      "Narendra Modi Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      "username": "adventure_seeker",
+      "userlocation": "Kyoto, Japan",
+      "userprofile": "https://images.unsplash.com/photo-1681562978415-bbf1dfe59353?q=80&w=2624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "userPost": "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "like": false,
+      "likeCount": 600,
+      "commentCount": 33,
+      "shareCount": 18,
+      "caption": "The serenity of Kyoto's bamboo forest. 🌳 #NatureLover",
+      "timeAgoUploaded": 18
   }
-];
-
-let pageContainer = document.querySelector(".page-container");
-
-users.forEach(function (elem) {
-  pageContainer.innerHTML += `     <div class="users">
-        <div class="user1">
-          <div class="id">
-            <img src="${elem.profile_pic}" alt="" />
-            <h5>${elem.name}</h5>
-          </div>
-          <div class="posts">
-            <img id="post1" src="${elem.post_image}" alt="" />
-            <i id="love" class="ri-heart-fill"></i>
-          </div>
-
-          <div class="post-icons">
-            <div class="like">
-              <i id="mini-heart" class="ri-heart-fill"></i>
-              <i class="ri-chat-3-line"></i>
-              <i class="ri-send-plane-line"></i>
-              <h6>${elem.likes}</h6>
-              <p>${elem.description}</p>
-            </div>
-
-            <div class="save">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000000"
-              >
-                <path
-                  d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>`;
-});
-
-var cont = document.querySelector(".page-container");
-var love = document.querySelector("#love");
-var posts = document.querySelector(".posts");
-var post1 = document.querySelector("#post1");
-var like = document.querySelector(".like");
-var miniheart = document.querySelector("#mini-heart");
-var images = document.querySelector(".images")
-var img = document.querySelector("image")
-
-// post1.addEventListener("dblclick", function (dets) {
-//   console.log([dets.target.id])
+]
+let posts = document.querySelector(".posts")
+function insta(){
+  let clutter = "";
   
-//   love.style.transform = `translate(-50%, -50%) scale(1)`;
-// });
+  users.forEach(function(val, index){
+    clutter += `    <div class="page">
+        <div class="profiles">
+        <img src="${val.userprofile}" alt="">
+        <div class="prof-name">
+        <h5>${val.username}</h5>
+        <p> <i class="ri-music-2-fill"></i> Harkirat Sangha, Akhiyan</p>
+      </div>
+    </div>
+    <div class="post1">
+      <img id="${index}" src="${val.userPost}" alt="">
+      <div class="reaction">
+        <div  class="likes">
+     ${val.like ? `   <i style="color :red" class="ri-heart-fill"></i>`: `  <i class="ri-heart-line"></i>`}
+       <p>${val.likeCount}</p>
+        <i class="ri-chat-3-line"></i>
+        <p>${val.commentCount}</p>
+        <i class="ri-send-plane-line"></i>
+          <p>${val.shareCount}</p>
+      </div>
+        <div class="save">
+           
+        </div>
+         <div class="description">
+          <p>${val.caption}</p>
+          <p>View all comments</p>
+        <p>${val.timeAgoUploaded} days ago</p>
+        </div>
+      </div>
+    </div>
 
-post1.addEventListener("dblclick", function () {
-  love.style.transform = `translate(-50%, -50%) scale(1)`;
-  miniheart.style.color = `red`;
-  love.style.fontSize = `50px`
-  setTimeout(function () {
-    love.style.transform = `translate(-50%, -50%) scale(0)`;
-  }, 1000);
-});
+        
+       
+      </div>`
 
+      posts.innerHTML = clutter
+      })
+}
+insta()
+
+posts.addEventListener("dblclick", function(dets){
+// console.log(users[dets.target.id]);
+if(!users[dets.target.id].like===true){
+users[dets.target.id].like=true
+users[dets.target.id].likeCount++
+
+}else{
+users[dets.target.id].like=false
+users[dets.target.id].likeCount--
+}
+insta()
+// console.log(users[dets.target.id].like)
+
+})
+posts.addEventListener("click", function(dets){
+  // console.log(users[dets.target.id]);
+  if(!users[dets.target.id-10].like===true){
+  users[dets.target.id-10].like=true
+  users[dets.target.id-10].likeCount++
+  
+  }else{
+  users[dets.target.id-10].like=false
+  users[dets.target.id-10].likeCount--
+  }
+  insta()
+  // console.log(users[dets.target.id].like)
+  
+  })
+id=`${index+10}`
